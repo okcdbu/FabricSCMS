9464731216e2551d5f9cd1aafd50214a71518a14
okcdbu <56333668+okcdbu@users.noreply.github.com>
2024-10-14 01:27:21 +0900

    cb-user update

--- repository/dashboard/dashboard.go
+++ repository/dashboard/dashboard.go
@@ -43,12 +43,12 @@ type Asset struct {
 var (
 	peerAddressOrg1 = "localhost:7051"
 	peerAddressOrg2 = "localhost:9051"
-	GOPATH          = "/home/ubuntu"
+	GOPATH          = "/home/cb-user"
 	networkPath     = fmt.Sprintf("%s/fabric-samples/test-network", GOPATH)
 	scriptPath      = fmt.Sprintf("%s/fabric-samples/test-network/scripts", GOPATH)
 	now             = time.Now()
 	assetId         = fmt.Sprintf("asset%d", now.Unix()*1e3+int64(now.Nanosecond()/1e6))
-	CCPATHROOT      = "/home/ubuntu/chaincodes"
+	CCPATHROOT      = "/home/cb-user/chaincodes"
 )
 
 // FileUpload
